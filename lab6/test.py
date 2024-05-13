3fac82841a5aeca720e74d56a86b69fc6f675443
mikoavelli <113817627+mikoavelli@users.noreply.github.com>
2024-05-13 16:13:14 +0300

    Update test.py

--- lab6/test.py
+++ lab6/test.py
@@ -53,11 +53,23 @@ class TestHashTable(unittest.TestCase):
         htable.create('8', 'Do 2')
         htable.create('9', 'Re 2')
         htable.create('banana', 'Mi 2')
+        htable.create('kamar', 'BANANA')
         self.assertEqual(htable.read('1'), 'Do 1')
         self.assertEqual(htable.read('2'), 'Re 1')
         self.assertEqual(htable.read('3'), 'Mi 1')
         self.assertEqual(htable.read('4'), 'Fa')
         self.assertEqual(htable.read('5'), 'Sol')
+
+    def test_str_method(self):
+        htable: hash_table.HashTable = hash_table.HashTable(10)
+        htable.create('1', 'Do 1')
+        htable.create('2', 'Re 1')
+        htable.create('3', 'Mi 1')
+        htable.create('4', 'Fa')
+        htable.create('4', 'Fa')
+        htable_str = ("1: ('4', 'Fa')\n" + "2: None\n" + "3: None\n" + "4: ('1', 'Do 1')\n" + "5: None\n" +
+                      "6: None\n" + "7: ('2', 'Re 1')\n" + "8: None\n" + "9: None\n" + "10: ('3', 'Mi 1')\n")
+        self.assertEqual(str(htable), htable_str)
 
 
 if __name__ == '__main__':
